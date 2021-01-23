28699a40056e60fa2399d91f72a287d6b52d1504
arq-gabo <jgabrielguerral@gmail.com>
2021-01-23 12:12:25 -0500

    Arreglos

---
+++ __test__/arrays.test.js
@@ -0,0 +1,13 @@
+import { arrayFruits, arrayColors } from '../arrays'
+
+describe('Comprobar que existe un elemento', () => {
+    test('Contiene banana', () => {
+        expect(arrayFruits()).toContain('banana')
+    })
+    test('No contiene platano', () => {
+        expect(arrayFruits()).not.toContain('platano')
+    })
+    test('Comprobar tamaño de un arreglo', () => {
+        expect(arrayFruits()).toHaveLength(6)
+    })
+})
