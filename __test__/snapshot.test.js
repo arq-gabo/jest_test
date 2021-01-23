eaa7f1dc9b059004119b5bb547a3e7f4646028bd
arq-gabo <jgabrielguerral@gmail.com>
2021-01-23 17:05:02 -0500

    Snapshot Testing

---
+++ __test__/snapshot.test.js
@@ -0,0 +1,10 @@
+'use strict'
+
+import { getCharacter } from '../snapshot'
+import rick from '../rick.json'
+
+describe('Es hora de las instantaneas', () => {
+    test('Snapshot', () => {
+        expect(getCharacter(rick)).toMatchSnapshot()
+    })
+})
---
+++ snapshot.js
@@ -0,0 +1,9 @@
+'use strict'
+
+export const getCharacter = (data) => ({
+    "id": data.id,
+    "name": data.name,
+    "status": data.status,
+    "species": data.species,
+    "gender": data.gender
+})
