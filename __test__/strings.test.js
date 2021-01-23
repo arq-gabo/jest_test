b4ea9ba48563b5669aaf8f47acdcc01ef5ea09eb
arq-gabo <jgabrielguerral@gmail.com>
2021-01-23 13:05:58 -0500

    Strings

---
+++ __test__/strings.test.js
@@ -0,0 +1,18 @@
+'use strict'
+
+describe('Comprobar cadenas de texto', () => {
+
+    const text = 'Un bonito texto'
+
+    test('Debe contener el siguiente texto', () => {
+        expect(text).toMatch(/bonito/)
+    })
+
+    test('No contiene el siguiente texto', () => {
+        expect(text).not.toMatch(/es/)
+    })
+
+    test('Comprobar el tamaño de un texto', () => {
+        expect(text).toHaveLength(15)
+    })
+})
